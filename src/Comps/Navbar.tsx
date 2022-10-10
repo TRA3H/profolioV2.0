451919404b098dd780253f20c469d6d439fa9134
Cyrus Baybay <41849568+TRA3H@users.noreply.github.com>
2022-10-09 22:54:47 -0700

    changed bg color of main page
    learned about tailwind css, next.js, and typescript lol

---
+++ src/Comps/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return(
+        <div>
+            <a href="#Home">Home</a>
+            <a>Contact</a>
+            <a>Not Listening to Anything</a>
+        </div>
+    );
+
+}
+
+export default Navbar
